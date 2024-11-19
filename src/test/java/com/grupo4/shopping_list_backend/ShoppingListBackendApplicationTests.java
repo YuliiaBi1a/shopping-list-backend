d456079d7132c00e8b4e0a28f819bcd2797ea192
Yuliia <jasmin.ic.km.ua13@gmail.com>
2024-11-19 15:34:11 +0100

    Initial commit

---
+++ src/test/java/com/grupo4/shopping_list_backend/ShoppingListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4.shopping_list_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
